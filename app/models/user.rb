71acc96467fec82f44a9df744672499f2c4e7dfe
Linell <tlbonnette@gmail.com>
2016-10-22 22:01:24 -0500

    feat: add user search
    Add the ability to search users via searchkick

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,23 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  # Searchkick Configuration                                                    {{{
+  # -------------------------------------------------------------------------------
+  searchkick autocomplete: ['full_name']
+
+  def search_data
+    as_json.merge(
+      {
+        'full_name' => full_name
+      }
+    )
+  end
+
+  # }}}
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
