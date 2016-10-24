dfbb4e11c68329d378a97b7ca1824ddef32504ab
Kyle Macey <kylemacey@github.com>
2016-10-24 19:28:31 -0400

    Add tests to verify unauthenticated redirects
    Ensure redirects for directories#index
    Ensure redirects for directories#show

--- test/controllers/directory_controller_test.rb
+++ test/controllers/directory_controller_test.rb
@@ -14,4 +14,14 @@ class DirectoryControllerTest < ActionController::TestCase
     get :show
     assert_response :success
   end
+
+  test "unauthenticated users should be redirected from index" do
+    get :index
+    assert_redirected_to controller: "users/sessions", action: "new"
+  end
+
+  test "unauthenticated users should be redirected from show" do
+    get :show
+    assert_redirected_to controller: "users/sessions", action: "new"
+  end
 end
